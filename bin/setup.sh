d54dc9827f7082a0d30c762629630a8131b4ef3b
Dave Rusek <dave.rusek@gmail.com>
2018-12-15 10:30:36 -0700

    Remove local file

--- bin/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home
-
-./dlog admin bind -l /ledgers -s 127.0.0.1:7000 -c distributedlog://127.0.0.1:7000/messaging/my_namespace
-./dlog tool create -u distributedlog://127.0.0.1:7000/messaging/my_namespace -r messaging-stream- -e 1-5
-
